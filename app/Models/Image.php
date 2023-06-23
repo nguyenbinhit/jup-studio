1d3ecfad6629eafabea622b21f4529744c9118a5
Nguyen Binh <69001423+nguyenbinhit@users.noreply.github.com>
2023-06-23 23:20:51 +0700

    create model and create database

---
+++ app/Models/Image.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Models;
+
+use App\Traits\Uuid;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Image extends Model
+{
+    use HasFactory, Uuid, SoftDeletes;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'name',
+        'alt',
+        'url',
+    ];
+
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var array<int, string>
+     */
+    protected $hidden = [
+        'id',
+        'uuid',
+    ];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array<string, string>
+     */
+    protected $casts = [
+        'name' => 'string',
+        'alt' => 'string',
+        'url' => 'string',
+    ];
+
+    public function employee(): BelongsTo
+    {
+        return $this->belongsTo(Employee::class);
+    }
+}
--- database/migrations/2023_06_23_155001_create_pages_table.php
+++ database/migrations/2023_06_23_155001_create_pages_table.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Enums\EmployeeStatus;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -12,16 +11,14 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('employees', function (Blueprint $table) {
+        Schema::create('pages', function (Blueprint $table) {
             $table->id();
             $table->uuid('uuid')->unique();
             $table->string('name');
-            $table->string('position')->nullable(); // Chức vụ
-            $table->text('description')->nullable();
-            $table->json('socials')->nullable();
-            $table->string('status')->default(EmployeeStatus::Publish->value);
+            $table->string('title');
+            $table->foreignId('image_id')->nullable()->constrained('images');
+            $table->text('description');
             $table->timestamps();
-            $table->softDeletes();
         });
     }
 
@@ -30,6 +27,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('employees');
+        Schema::dropIfExists('pages');
     }
 };
