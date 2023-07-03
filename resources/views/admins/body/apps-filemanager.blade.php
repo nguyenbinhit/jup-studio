5bc0aa037d8cc982153a851f19df4a17aa5ad0f1
Nguyen Binh <69001423+nguyenbinhit@users.noreply.github.com>
2023-07-03 23:01:23 +0700

    fix load image not found

--- resources/views/admins/body/apps-filemanager.blade.php
+++ resources/views/admins/body/apps-filemanager.blade.php
@@ -1,10 +1,8 @@
 ﻿@extends('admins.index')
 @section('content')
     <div class="content">
-
         <!-- Start Content-->
         <div class="container-fluid">
-
             <!-- start page title -->
             <div class="row">
                 <div class="col-12">
@@ -12,7 +10,6 @@
                         <div class="page-title-right">
                             <ol class="breadcrumb m-0">
                                 <li class="breadcrumb-item"><a href="javascript: void(0);">Jup studio</a></li>
-                                <li class="breadcrumb-item"><a href="javascript: void(0);">Ứng dụng</a></li>
                                 <li class="breadcrumb-item active">Quản lý tập tin</li>
                             </ol>
                         </div>
@@ -35,17 +32,18 @@
                                     </tr>
                                 </thead>
                                 <tbody>
-                                    @foreach ($images as $key => $item)
+                                    @foreach ($images as $image)
                                         <tr>
                                             <td>
-                                                <img src="{{ $item->url }}" height="30" alt="icon" class="mr-2">
-                                                <a href="javascript:void(0);" class="text-dark">{{ $item->alt }}</a>
+                                                <img src="{{ asset('../..' . Storage::url($image->url)) }}" height="30"
+                                                    alt="icon" class="mr-2">
+                                                <a href="javascript:void(0);" class="text-dark">{{ $image->alt }}</a>
                                             </td>
-                                            <td class="text-muted font-13">{{ $item->updated_at }}</td>
+                                            <td class="text-muted font-13">{{ $image->created_at }}</td>
 
                                             <td>
                                                 @php
-                                                    $sizeInBytes = $item->size;
+                                                    $sizeInBytes = $image->size;
                                                     $sizeInKB = round($sizeInBytes / 1024, 2);
                                                     $sizeInMB = round($sizeInKB / 1024, 2);
                                                     $displaySize = $sizeInMB > 1 ? $sizeInMB . 'MB' : $sizeInKB . 'KB';
@@ -54,7 +52,6 @@
                                             </td>
                                         </tr>
                                     @endforeach
-
                                 </tbody>
                             </table>
                         </div>
@@ -63,7 +60,15 @@
             </div>
             <!-- end row -->
 
+            <div class="row mt-3">
+                <div class="col-12">
+                    <div class="text-right">
+                        <div class="d-flex justify-content-end">
+                            {!! $images->links() !!}
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div> <!-- container -->
-
     </div> <!-- content -->
 @endsection
