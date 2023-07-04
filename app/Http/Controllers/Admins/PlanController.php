cee70469af4ed62b818ebc09fdff20bf50c13d7b
vuvantuyen441 <91613734+vuvantuyen441@users.noreply.github.com>
2023-07-04 23:59:52 +0700

    create price

--- app/Http/Controllers/Admins/PlanController.php
+++ app/Http/Controllers/Admins/PlanController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Admins;
 
 use App\Http\Controllers\BaseController;
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Admins\Plan\StoreRequest;
 use App\Models\Plan;
 use Illuminate\Contracts\View\View;
 use Illuminate\Http\Request;
@@ -15,13 +16,13 @@ class PlanController extends BaseController
      */
     public function index()
     {
-
         return view('admins.body.extras-pricing',
         ['prices'=> Plan::all()]);
     }
 
     /**
      * Show the form for creating a new resource.
+     *
      */
     public function create()
     {
@@ -30,10 +31,15 @@ class PlanController extends BaseController
 
     /**
      * Store a newly created resource in storage.
+     * @param StoreRequest $request
+     * @return View
      */
-    public function store(Request $request)
+    public function store(StoreRequest $request): View
     {
-       logger($request);
+        $data = $request->safe()->all();
+        $plan = Plan::create($data);
+
+        return view('admins.body.plan.update-pricing',['plan' => $plan]);
     }
 
     /**
--- resources/views/admins/body/plan/create-pricing.blade.php
+++ resources/views/admins/body/plan/create-pricing.blade.php
@@ -1,124 +1,125 @@
 @extends('admins.index')
 @section('content')
-<div class="content">
-    <!-- Start Content-->
-    <div class="container-fluid">
-        <!-- start page title -->
-        <div class="row">
-            <div class="col-12">
-                <div class="page-title-box">
-                    <div class="page-title-right">
-                        <ol class="breadcrumb m-0">
-                            <li class="breadcrumb-item"><a href="javascript: void(0);">Minton</a></li>
-                            <li class="breadcrumb-item active">Thêm mới bảng giá</li>
-                        </ol>
+    <div class="content">
+        <!-- Start Content-->
+        <div class="container-fluid">
+            <!-- start page title -->
+            <div class="row">
+                <div class="col-12">
+                    <div class="page-title-box">
+                        <div class="page-title-right">
+                            <ol class="breadcrumb m-0">
+                                <li class="breadcrumb-item"><a href="javascript: void(0);">Minton</a></li>
+                                <li class="breadcrumb-item active">Thêm mới bảng giá</li>
+                            </ol>
+                        </div>
+                        <h4 class="page-title">Thêm mới bảng giá</h4>
                     </div>
-                    <h4 class="page-title">Thêm mới bảng giá</h4>
                 </div>
             </div>
-        </div>
-        <!-- end page title -->
+            <!-- end page title -->
 
-        <div class="row">
-            <div class="col-12">
-                <div class="card-box">
-                    <ul class="nav nav-pills navtab-bg">
-                        <li class="nav-item">
-                            <a href="#" data-toggle="tab" aria-expanded="true" class="nav-link active">
-                                <i class="mdi mdi-settings-outline mr-1"></i>Thêm mới
-                            </a>
-                        </li>
-                    </ul>
+            <div class="row">
+                <div class="col-12">
+                    <div class="card-box">
+                        <ul class="nav nav-pills navtab-bg">
+                            <li class="nav-item">
+                                <a href="#" data-toggle="tab" aria-expanded="true" class="nav-link active">
+                                    <i class="mdi mdi-settings-outline mr-1"></i>Thêm mới
+                                </a>
+                            </li>
+                        </ul>
 
-                    <div class="tab-content">
-                        <div class="tab-pane show active" id="settings">
-                            <form method="POST" action="{{ route('admin.plan.store')" class="needs-validation" novalidate="" enctype="multipart/form-data">  @csrf
-                                <h5 class="mb-3 text-uppercase bg-light p-2"><i class="mdi mdi-account-circle mr-1"></i>
-                                    Thông tin bảng giá</h5>
-                                <div class="row">
-                                    <div class="col-md-6">
-                                        <div class="form-group ">
-                                            <label for="nameprice">Tên bảng giá</label>
-                                            <input type="text" class="form-control" id="name" name="name"
-                                                placeholder="Tên bảng giá" value="" required="">
-                                            <div class="invalid-feedback">
-                                                Vui lòng nhập tên bảng giá!
+                        <div class="tab-content">
+                            <div class="tab-pane show active" id="settings">
+                                <form method="POST" action="{{ route('admin.plans.store')}}" class="needs-validation"
+                                     enctype="multipart/form-data"> @csrf
+                                    <h5 class="mb-3 text-uppercase bg-light p-2"><i class="mdi mdi-account-circle mr-1"></i>
+                                        Thông tin bảng giá</h5>
+                                    <div class="row">
+                                        <div class="col-md-6">
+                                            <div class="form-group ">
+                                                <label for="nameprice">Tên bảng giá</label>
+                                                <input type="text" class="form-control" id="name" name="name"
+                                                    placeholder="Tên bảng giá" value="" required="">
+                                                <div class="invalid-feedback">
+                                                    Vui lòng nhập tên bảng giá!
+                                                </div>
+                                            </div>
+                                        </div>
+
+                                        <div class="col-md-6">
+                                            <div class="form-group">
+                                                <label for="price">Giá</label>
+                                                <input type="number" class="form-control" id="price" name="price"
+                                                    placeholder="Giá" value="" required="">
+                                                <div class="invalid-feedback">
+                                                    Vui lòng nhập số lượng giá!
+                                                </div>
                                             </div>
                                         </div>
                                     </div>
+                                    <div class="row">
+                                        <div class="col-md-12">
+                                            <label for="description">Miêu tả</label>
+                                            <textarea class="form-control" id="summernote-editor" name="description" rows="4"
+                                                placeholder="Viết mô tả về bảng giá..."></textarea>
+                                        </div>
+                                    </div>
+                                    <h5 class="mb-3 text-uppercase bg-light p-2 mt-3"><i
+                                            class="mdi mdi-account-circle mr-1"></i>
+                                        Lợi ích chính</h5>
+                                    <div class="row">
+                                        <div class="col-md-6">
+                                            <div class="form-group">
+                                                <label for="first">Một</label>
+                                                <input type="text" class="form-control" id="first" name="benefits[first]"
+                                                    placeholder="" value="">
 
-                                    <div class="col-md-6">
-                                        <div class="form-group">
-                                            <label for="validationCustom02">Giá</label>
-                                            <input type="number" class="form-control" id="validationCustom02"
-                                                placeholder="Giá" value="" required="">
-                                            <div class="invalid-feedback">
-                                                Vui lòng nhập số lượng giá!
+                                            </div>
+                                        </div>
+                                        <div class="col-md-6">
+                                            <div class="form-group">
+                                                <label for="second">Hai</label>
+                                                <input type="text" class="form-control" id="second" name="benefits[second]"
+                                                    placeholder="" value="">
+
                                             </div>
                                         </div>
                                     </div>
-                                </div>
-                                <div class="row">
-                                    <div class="col-md-12">
-                                        <label for="validationCustom01">Miêu tả</label>
-                                        <textarea class="form-control"></textarea>
-                                    </div>
-                                </div>
-                                <h5 class="mb-3 text-uppercase bg-light p-2 mt-3"><i
-                                        class="mdi mdi-account-circle mr-1"></i>
-                                    Lợi ích</h5>
-                                <div class="row">
-                                    <div class="col-md-6">
-                                        <div class="form-group">
-                                            <label for="validationCustom02">Một</label>
-                                            <input type="text" class="form-control" id="validationCustom02"
-                                                placeholder="" value="">
+                                    <div class="row">
+                                        <div class="col-md-6">
+                                            <div class="form-group">
+                                                <label for="third">Ba</label>
+                                                <input type="text" class="form-control" id="third" name="benefits[third]"
+                                                    placeholder="" value="">
 
+                                            </div>
+                                        </div>
+                                        <div class="col-md-6">
+                                            <div class="form-group">
+                                                <label for="fourth" name="fourth">Bốn</label>
+                                                <input type="text" class="form-control" id="fourth" name="benefits[fourth]"
+                                                    placeholder="" value="">
+                                            </div>
                                         </div>
                                     </div>
-                                    <div class="col-md-6">
-                                        <div class="form-group">
-                                            <label for="validationCustom02">Hai</label>
-                                            <input type="text" class="form-control" id="validationCustom02"
-                                                placeholder="" value="">
-
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="row">
-                                    <div class="col-md-6">
-                                        <div class="form-group">
-                                            <label for="validationCustom02">Ba</label>
-                                            <input type="text" class="form-control" id="validationCustom02"
-                                                placeholder="" value="">
-
-                                        </div>
-                                    </div>
-                                    <div class="col-md-6">
-                                        <div class="form-group">
-                                            <label for="validationCustom02">Bốn</label>
-                                            <input type="text" class="form-control" id="validationCustom02"
-                                                placeholder="" value="">
-
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="text-left">
+                                    <div class="text-left">
                                         <button type="submit" class="btn btn-success waves-effect waves-light mt-2"><i
                                                 class="mdi mdi-content-save"></i> Lưu</button>
                                     </div>
-                            </form>
-                        </div>
-                        <!-- end settings content-->
+                                </form>
+                            </div>
+                            <!-- end settings content-->
 
-                    </div> <!-- end tab-content -->
-                </div> <!-- end card-box-->
+                        </div> <!-- end tab-content -->
+                    </div> <!-- end card-box-->
 
-            </div> <!-- end col -->
-        </div>
-        <!-- end row-->
+                </div> <!-- end col -->
+            </div>
+            <!-- end row-->
 
-    </div> <!-- container -->
+        </div> <!-- container -->
 
-</div> <!-- content -->
+    </div> <!-- content -->
 @endsection
