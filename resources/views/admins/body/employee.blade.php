75404ebdccc798e9d93dec971f08e8d9b3c69745
Nguyen Binh <69001423+nguyenbinhit@users.noreply.github.com>
2023-07-28 22:49:52 +0700

    update start page title

--- resources/views/admins/body/employee.blade.php
+++ resources/views/admins/body/employee.blade.php
@@ -10,7 +10,7 @@
                     <div class="page-title-box">
                         <div class="page-title-right">
                             <ol class="breadcrumb m-0">
-                                <li class="breadcrumb-item"><a href="javascript: void(0);">JupStudio</a></li>
+                                <li class="breadcrumb-item"><a href="{{ route('admin.index') }}">{{ env('WEBSITE_NAME') }}</a></li>
                                 <li class="breadcrumb-item active">Thành viên studio</li>
                             </ol>
                         </div>
--- resources/views/admins/body/user/update-user.blade.php
+++ resources/views/admins/body/user/update-user.blade.php
@@ -9,8 +9,10 @@
                     <div class="page-title-box">
                         <div class="page-title-right">
                             <ol class="breadcrumb m-0">
-                                <li class="breadcrumb-item"><a href="javascript: void(0);">{{ env('WEBSITE_NAME') }}</a></li>
-                                <li class="breadcrumb-item"><a href="javascript: void(0);">Quản lý tài khoản</a></li>
+                                <li class="breadcrumb-item"><a href="{{ route('admin.index') }}">{{ env('WEBSITE_NAME') }}</a>
+                                </li>
+                                <li class="breadcrumb-item"><a href="{{ route('admin.users.index') }}">Quản lý tài khoản</a>
+                                </li>
                                 <li class="breadcrumb-item active">Hồ sơ</li>
                             </ol>
                         </div>
