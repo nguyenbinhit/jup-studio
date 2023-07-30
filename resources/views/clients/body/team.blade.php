9a46b8c61225eca34c9f433dcaa511ad729edc43
Nguyen Binh <69001423+nguyenbinhit@users.noreply.github.com>
2023-07-30 21:27:56 +0700

    Update team.blade.php

--- resources/views/clients/body/team.blade.php
+++ resources/views/clients/body/team.blade.php
@@ -10,14 +10,12 @@
              </div>
          </div>
          <div class="row wow fadeInUp team" data-wow-delay="300">
-
              @foreach ($employees as $key => $item)
-
                  @if ($key % 2 == 0)
                      <div class="col-lg-6 col-xl-3 p-0  ml-md-auto mr-md-0 mx-auto">
                          <div class="team-image">
-                             <img src="{{ $item->image?->url ? asset('../..' . Storage::url($item->image->url)) : asset('../bootstrapv1/img/team1.jpg') }}" alt="team1"
-                                 class="m-image{{ $key + 1 }}">
+                             <img src="{{ $item->image?->url ? asset('../..' . Storage::url($item->image->url)) : asset('../bootstrapv1/img/team1.jpg') }}"
+                                 alt="team1" class="m-image{{ $key + 1 }}">
                          </div>
                          <div class="team-classic-content  pink">
                              <h3 class="mb-3 text-capitalize color-black">{{ $item->name }}</h3>
@@ -25,7 +23,7 @@
                              <p class="mt-3 mb-3 color-black">{{ $item->description }}</p>
                              <ul class="team-social">
                                  @foreach ($item->socials as $key => $value)
-                                 @if ($key === 'facebook' && $item->socials['facebook'])
+                                     @if ($key === 'facebook' && $item->socials['facebook'])
                                          <li class="d-inline-block"><a href="javascript:void(0)"
                                                  class="facebook-bg-hvr"><i class="fab fa-facebook-f"
                                                      aria-hidden="true"></i></a></li>
@@ -49,29 +47,33 @@
                          <div class="row">
                              <div class="col-md-12 order-md-1">
                                  <div class="team-image">
-                                     <img src="{{ asset('../bootstrapv1/img/team2.jpg') }}" alt="team2"
-                                         class="m-image2">
+                                     <img src="{{ $item->image?->url ? asset('../..' . Storage::url($item->image->url)) : asset('../bootstrapv1/img/team1.jpg') }}"
+                                         alt="team1" class="m-image{{ $key + 1 }}">
                                  </div>
                              </div>
                              <div class="col-md-12 order-md-0">
                                  <div class="team-classic-content  blue">
-                                     <h3 class="mb-3 text-capitalize color-black">Rob
-                                         Clark</h3>
-                                     <h5 class="mb-3 text-capitalize color-blue">marketing
-                                         head</h5>
-                                     <p class="mt-3 mb-3 color-black">Lorem ipsum
-                                         dolor sit amet, consectetur adipiscing
-                                         elit augue.</p>
+                                     <h3 class="mb-3 text-capitalize color-black">{{ $item->name }}</h3>
+                                     <h5 class="mb-3 text-capitalize color-blue">{{ $item->position }}</h5>
+                                     <p class="mt-3 mb-3 color-black">{{ $item->description }}</p>
                                      <ul class="team-social">
-                                         <li class="d-inline-block"><a href="javascript:void(0)"
-                                                 class="facebook-bg-hvr"><i class="fab fa-facebook-f"
-                                                     aria-hidden="true"></i></a></li>
-                                         <li class="d-inline-block"><a href="javascript:void(0)"
-                                                 class="twitter-bg-hvr"><i class="fab fa-twitter"
-                                                     aria-hidden="true"></i></a></li>
-                                         <li class="d-inline-block"><a href="javascript:void(0)"
-                                                 class="google-bg-hvr"><i class="fab fa-google"
-                                                     aria-hidden="true"></i></a></li>
+                                         @foreach ($item->socials as $key => $value)
+                                             @if ($key === 'facebook' && $item->socials['facebook'])
+                                                 <li class="d-inline-block"><a href="javascript:void(0)"
+                                                         class="facebook-bg-hvr"><i class="fab fa-facebook-f"
+                                                             aria-hidden="true"></i></a></li>
+                                             @endif
+                                             @if ($key === 'twitter' && $item->socials['twitter'])
+                                                 <li class="d-inline-block"><a href="javascript:void(0)"
+                                                         class="twitter-bg-hvr"><i class="fab fa-twitter"
+                                                             aria-hidden="true"></i></a></li>
+                                             @endif
+                                             @if ($key === 'google' && $item->socials['google'])
+                                                 <li class="d-inline-block"><a href="javascript:void(0)"
+                                                         class="google-bg-hvr"><i class="fab fa-google"
+                                                             aria-hidden="true"></i></a></li>
+                                             @endif
+                                         @endforeach
                                      </ul>
                                  </div>
                              </div>
@@ -79,105 +81,6 @@
                      </div>
                  @endif
              @endforeach
-
-             {{-- <div class="col-lg-6 col-xl-3 p-0  ml-md-auto mr-md-0 mx-auto ">
-                 <div class="team-image">
-                     <img src="{{ asset('../bootstrapv1/img/team3.jpg') }}" alt="team3" class="m-image3">
-                 </div>
-                 <div class="team-classic-content  yellow">
-                     <h3 class="mb-3 text-capitalize color-black">Nicole
-                         Cross</h3>
-                     <h5 class="mb-3 text-capitalize color-yellow">Visualizer</h5>
-                     <p class="mt-3 mb-3 color-black">Lorem ipsum dolor
-                         sit amet, consectetur adipiscing elit augue.</p>
-                     <ul class="team-social">
-                         <li class="d-inline-block"><a href="javascript:void(0)" class="facebook-bg-hvr"><i
-                                     class="fab fa-facebook-f" aria-hidden="true"></i></a></li>
-                         <li class="d-inline-block"><a href="javascript:void(0)" class="twitter-bg-hvr"><i
-                                     class="fab fa-twitter" aria-hidden="true"></i></a></li>
-                         <li class="d-inline-block"><a href="javascript:void(0)" class="google-bg-hvr"><i
-                                     class="fab fa-google" aria-hidden="true"></i></a></li>
-                     </ul>
-                 </div>
-             </div>
-             <div class="col-lg-6 col-xl-3 p-0  mr-md-auto ml-md-0 mx-auto">
-                 <div class="row">
-                     <div class="col-md-12 order-md-1">
-                         <div class="team-image">
-                             <img src="{{ asset('../bootstrapv1/img/team2.jpg') }}" alt="team2" class="m-image2">
-                         </div>
-                     </div>
-                     <div class="col-md-12 order-md-0">
-                         <div class="team-classic-content  blue">
-                             <h3 class="mb-3 text-capitalize color-black">Rob
-                                 Clark</h3>
-                             <h5 class="mb-3 text-capitalize color-blue">marketing
-                                 head</h5>
-                             <p class="mt-3 mb-3 color-black">Lorem ipsum
-                                 dolor sit amet, consectetur adipiscing
-                                 elit augue.</p>
-                             <ul class="team-social">
-                                 <li class="d-inline-block"><a href="javascript:void(0)" class="facebook-bg-hvr"><i
-                                             class="fab fa-facebook-f" aria-hidden="true"></i></a></li>
-                                 <li class="d-inline-block"><a href="javascript:void(0)" class="twitter-bg-hvr"><i
-                                             class="fab fa-twitter" aria-hidden="true"></i></a></li>
-                                 <li class="d-inline-block"><a href="javascript:void(0)" class="google-bg-hvr"><i
-                                             class="fab fa-google" aria-hidden="true"></i></a></li>
-                             </ul>
-                         </div>
-                     </div>
-                 </div>
-             </div>
-             <div class="w-100 d-none d-lg-block d-xl-none"></div>
-             <div class="col-lg-6 col-xl-3 p-0  ml-md-auto mr-md-0 mx-auto ">
-                 <div class="team-image">
-                     <img src="{{ asset('../bootstrapv1/img/team3.jpg') }}" alt="team3" class="m-image3">
-                 </div>
-                 <div class="team-classic-content  yellow">
-                     <h3 class="mb-3 text-capitalize color-black">Nicole
-                         Cross</h3>
-                     <h5 class="mb-3 text-capitalize color-yellow">Visualizer</h5>
-                     <p class="mt-3 mb-3 color-black">Lorem ipsum dolor
-                         sit amet, consectetur adipiscing elit augue.</p>
-                     <ul class="team-social">
-                         <li class="d-inline-block"><a href="javascript:void(0)" class="facebook-bg-hvr"><i
-                                     class="fab fa-facebook-f" aria-hidden="true"></i></a></li>
-                         <li class="d-inline-block"><a href="javascript:void(0)" class="twitter-bg-hvr"><i
-                                     class="fab fa-twitter" aria-hidden="true"></i></a></li>
-                         <li class="d-inline-block"><a href="javascript:void(0)" class="google-bg-hvr"><i
-                                     class="fab fa-google" aria-hidden="true"></i></a></li>
-                     </ul>
-                 </div>
-             </div>
-             <div class="col-lg-6 col-xl-3 p-0  mr-md-auto ml-md-0 mx-auto ">
-                 <div class="row">
-                     <div class="col-md-12 order-md-1">
-                         <div class="team-image2">
-                             <img src="{{ asset('../bootstrapv1/img/team4.jpg') }}" alt="team4" class="m-image2">
-                         </div>
-                     </div>
-                     <div class="col-md-12 order-md-0">
-                         <div class="team-classic-content  pink">
-                             <h3 class="mb-3 text-capitalize color-black">Albert
-                                 Rodricks</h3>
-                             <h5 class="mb-3 text-capitalize color-pink">Creative
-                                 Lead</h5>
-                             <p class="mt-3 mb-3 color-black">Lorem ipsum
-                                 dolor sit amet, consectetur adipiscing
-                                 elit augue.</p>
-                             <ul class="team-social">
-                                 <li class="d-inline-block"><a href="javascript:void(0)" class="facebook-bg-hvr"><i
-                                             class="fab fa-facebook-f" aria-hidden="true"></i></a></li>
-                                 <li class="d-inline-block"><a href="javascript:void(0)" class="twitter-bg-hvr"><i
-                                             class="fab fa-twitter" aria-hidden="true"></i></a></li>
-                                 <li class="d-inline-block"><a href="javascript:void(0)" class="google-bg-hvr"><i
-                                             class="fab fa-google" aria-hidden="true"></i></a></li>
-                             </ul>
-                         </div>
-                     </div>
-                 </div>
-             </div> --}}
-
          </div>
      </div>
  </section>
