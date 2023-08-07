965babb95c142621d22e0756c8252b80faa51a20
Nguyen Binh <69001423+nguyenbinhit@users.noreply.github.com>
2023-08-07 22:42:07 +0700

    update css

--- resources/views/clients/body/contact.blade.php
+++ resources/views/clients/body/contact.blade.php
@@ -80,6 +80,14 @@
                 </a>
             </div>
             <div class="col-sm-12 col-md-6 p-0 col-map box-shadow-map">
+                <style type="text/css">
+                    @media only screen and (max-width: 600px) {
+                        iframe {
+                            width: 100%;
+                            height: 350px;
+                        }
+                    }
+                </style>
                 <div class="bg-light-gray map" style="width: 100%; height: 100%;">
                     <iframe
                         src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d7449.48015309319!2d105.73630426801837!3d21.00305377483419!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x313453d4ead47a63%3A0x9af352cfc1f411a1!2sVinhomes%20Smart%20City%20T%C3%A2y%20M%E1%BB%97!5e0!3m2!1svi!2s!4v1689522834113!5m2!1svi!2s"
